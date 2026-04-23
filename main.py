47389a90a8d5af57c21b29e9531497da4073d510
Isabelle Silva <bellinha292@gmail.com>
2026-04-23 19:50:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from exa_py import Exa
 
 exa = Exa('YOUR_KEY_HERE')
 query = input('Search here: ')
+
 response = exa.search(
-  'Best Chicago cold brew',
-  num_results=10,
+  query,
+  num_results=5,
+  type='keyword',
+  include_domains=['https://www.tiktok.com'],
 )
+
+for result in response.results:
+  print(f'Title: {result.title}')
+  print(f'URL: {result.url}')
+  print()
